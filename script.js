eea146f3ee940967695f57b2e8333676085bc748
Isabela Biazin <biazinisabela28@gmail.com>
2025-03-29 17:54:17 -0300

    TO DO LIST

---
+++ script.js
@@ -0,0 +1,23 @@
+//estado da aplicação
+const tarefas = ['coisa 1', 'coisa 2']
+
+function add() {
+    const input = document.querySelector("input")
+    const tarefa = input.value
+    tarefas.push(tarefa)
+    input.value = ""
+    render()
+}
+
+function render() {
+    const ul = document.querySelector("ul")
+    ul.innerHTML = null
+
+    tarefas.forEach(function (tarefa){
+        const li = document.createElement("li")
+        li.innerText = tarefa
+        ul.appendChild(li)
+    })
+    
+}
+render()
